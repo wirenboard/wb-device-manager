4b3e28c260e63f309f7ef6a78bb3e9ef42dd17e9
vdromanov <vdromanov@yahoo.com>
2022-11-23 12:51:26 +0300

    busscanner tests are async

--- tests/busscanner_test.py
+++ tests/busscanner_test.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import AsyncMock
 
 from wb.device_manager import serial_bus
 from wb_modbus import minimalmodbus
@@ -11,11 +11,11 @@ from wb_modbus import minimalmodbus
 class DummyScanner(serial_bus.WBExtendedModbusScanner):
 
     def __init__(self, *args, **kwargs):
-        self.instrument = MagicMock()
+        self.instrument = AsyncMock()
         self.port = kwargs.get("port", "/dev/dummyport")
 
 
-class TestMBExtendedScanner(unittest.TestCase):
+class TestMBExtendedScanner(unittest.IsolatedAsyncioTestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -24,55 +24,49 @@ class TestMBExtendedScanner(unittest.TestCase):
     @classmethod
     def mock_response(cls, response_hex_str):
         ret = minimalmodbus._hexdecode(response_hex_str)
-        cls.scanner.instrument._communicate = MagicMock(return_value=ret)  # we have no actual serial devices
+        cls.scanner.instrument._communicate = AsyncMock(return_value=ret)  # we have no actual serial devices
 
-    def test_correct_response(self):
+    async def test_correct_response(self):
         assumed_response = "FE34359603"
         self.mock_response(
             "fffffffffffffffffd6003fe34359603f6a80000000000000000"
         )
-        ret = self.scanner.get_next_device_data()
+        ret = await self.scanner.get_next_device_data()
         ret = minimalmodbus._hexencode(ret)
         self.assertEqual(assumed_response, ret)
 
-    def test_correct_scan_init(self):
+    async def test_correct_scan_init(self):
         self.mock_response("")  # No answer to broadcast scan-init cmd
-        self.scanner.init_bus_scan()
+        await self.scanner.init_bus_scan()
 
-    def test_correct_scan_end(self):
+    async def test_correct_scan_end(self):
         operation_code = "04"
         self.mock_response(
             "fffffffd60" + operation_code + "c9f3000000"
         )
-        ret = self.scanner.get_next_device_data()
+        ret = await self.scanner.get_next_device_data()
         self.assertIsNone(ret)
 
-    def test_unsupported_operation_code(self):
+    async def test_unsupported_operation_code(self):
         unsupported_code = "11"
         self.mock_response(
             "fffffffffffffffffd60" + unsupported_code + "fe34359603f6a80000000000000000"
         )
-        self.assertRaises(
-            minimalmodbus.InvalidResponseError,
-            lambda: self.scanner.get_next_device_data()
-        )
+        with self.assertRaises(minimalmodbus.InvalidResponseError):
+            await self.scanner.get_next_device_data()
 
-    def test_empty_answer(self):
-        self.mock_response("")
-        self.assertRaises(  # TODO: empty rpc-answer means no support in wb-mqtt-serial; maybe custom error?
-            minimalmodbus.InvalidResponseError,
-            lambda: self.scanner.get_next_device_data()
-        )
+    async def test_empty_answer(self):
+        self.mock_response("")  # TODO: empty rpc-answer means no support in wb-mqtt-serial; maybe custom error?
+        with self.assertRaises(minimalmodbus.InvalidResponseError):
+            await self.scanner.get_next_device_data()
 
-    def test_corrupted_answer(self):
+    async def test_corrupted_answer(self):
         incorrect_crc = "ffff"
         self.mock_response(
             "fffffffffd6003fe34359603" + incorrect_crc + "00000000"
         )
-        self.assertRaises(
-            minimalmodbus.InvalidResponseError,
-            lambda: self.scanner.get_next_device_data()
-        )
+        with self.assertRaises(minimalmodbus.InvalidResponseError):
+            await self.scanner.get_next_device_data()
 
 
 if __name__ == '__main__':
