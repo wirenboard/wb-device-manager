0bf4a1b7cd69a2ff0be87325ca38e325102deb65
Nikolay Korotkiy <sikmir@disroot.org>
2023-07-25 11:12:44 +0400

    Fix pkg-info, add LICENSE (#29)

--- setup.py
+++ setup.py
@@ -2,12 +2,21 @@
 
 from setuptools import setup
 
+
+def get_version():
+    with open("debian/changelog", "r", encoding="utf-8") as f:
+        return f.readline().split()[1][1:-1]
+
+
 setup(
     name="wb-device-manager",
-    version="0.1",
+    version=get_version(),
     author="Vladimir Romanov",
     author_email="v.romanov@wirenboard.ru",
+    maintainer="Wiren Board Team",
+    maintainer_email="info@wirenboard.com",
     description="Wiren Board modbus devices manager",
+    license="MIT",
     url="https://github.com/wirenboard/wb-device-manager",
     packages=[
         "wb.device_manager",
