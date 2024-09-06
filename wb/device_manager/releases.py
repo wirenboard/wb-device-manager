6ede8f778c385adb7979d34a478e5105289dcf38
KraPete <86825564+KraPete@users.noreply.github.com>
2024-09-06 08:30:40 +0500

    Update wb/device_manager/releases.py
    Co-authored-by: Nikolay Korotkiy <sikmir@disroot.org>

--- wb/device_manager/releases.py
+++ wb/device_manager/releases.py
@@ -38,7 +38,7 @@ def parse_fw_version(endpoint_url: str) -> str:
     """
     Parsing fw version from endpoint url, stored in releases file
     """
-    re_str = ".+/(.+)\\.wbfw"
+    re_str = r'.+/(.+)\.wbfw'
     mat = re.match(re_str, endpoint_url)  # matches .../*.wbfw
     if mat:
         return str(mat.group(1))
